45b33b7ffe973ba77e52534d0b829c94057aacf9
valrmzl <val@mail.com>
2022-11-24 14:27:23 -0600

    funcionamiento POST a la BD VAL

--- CODE/BACKEND/server.mjs
+++ CODE/BACKEND/server.mjs
@@ -1,10 +1,5 @@
 import express from "express";
 import mongoose from "mongoose";
-<<<<<<< HEAD
-let mongoConnectionUsers= "mongodb+srv://admin:van12210@proyectofinal.hx0n1h1.mongodb.net/ProyectoDB";
-let db=mongoose.connection;
-=======
->>>>>>> 09d7bb50d9ffcdb3d5263cdf13c3994c415f28f3
 import chalk from "chalk";
 import * as fs from "node:fs";
 import cors from "cors";
@@ -13,7 +8,7 @@ import randomize from "randomatic";
 const app = express();
 const port = 3000;
 let mongoConnectionUsers =
-    "mongodb+srv://admin:van12210@proyectofinal.hx0n1h1.mongodb.net/ProyectoFinal";
+    "mongodb+srv://admin:van12210@proyectofinal.hx0n1h1.mongodb.net/ProyectoDB";
 let db = mongoose.connection;
 
 app.use(
@@ -75,13 +70,10 @@ let userSchema = mongoose.Schema({
         required: true,
     },
 });
-<<<<<<< HEAD
-=======
 
 // D A T A B A S E
-let User = mongoose.model("users", userSchema);
 
->>>>>>> 09d7bb50d9ffcdb3d5263cdf13c3994c415f28f3
+
 app.use(express.json());
 
 const autenticar = (req, res, next) => {
@@ -93,12 +85,9 @@ app.use("/api/users", autenticar);
 app.use("/api/tarea", autenticar);
 // D A T A B A S E 
 
-<<<<<<< HEAD
 
 ///POST DE UN NIEVO USUARIO A LA BASE DE DATOS 
 let User= mongoose.model('users', userSchema); //el User hace referencia a qen que parte de la base se va a gaurdar 
-=======
->>>>>>> 09d7bb50d9ffcdb3d5263cdf13c3994c415f28f3
 app.post("/api/users", (req, res) => {
     res.send("Haciendo un POST de un nuevo usuario");
 
